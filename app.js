a4115ca914fd24fc8e4f913c7807de9a00f59051
jonghyun <godaiys@hotmail.com>
2019-08-22 21:32:06 -0400

    EJS templating

--- app.js
+++ app.js
@@ -4,39 +4,72 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+app.use(express.static("public"));
+
+//root route //
 app.get("/", function(req, res) {
 
   let today = new Date();
 
   let options = {
-    weekday:"long",
+    weekday: "long",
     day: "numeric",
     month: "long"
   };
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {
-    kindOfDay: day,
+  res.render("list.ejs", {
+    listTitle: day,
     newListItems: items
   });
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body);
+
+  //let item = req.body.newItem;
+
+  if (req.body.list === "work") { // check < name="list" value= <%= listTitle %> >.....if name is "list" and value starts with "work"
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+});
+
+// work page //
+
+app.get("/work", function(req, res) {
+  res.render("list.ejs", {
+    listTitle: "work list",
+    newListItems: workItems
+  })
+});
+
+app.post("/work", function(req, res) {
+
+  //let item = req.body.newItem;
+
+  workItems.push(req.body.newItem);
+  res.redirect("/work");
 
 });
 
-app.post("/", function(req, res){
-  let item = req.body.newItem;
+// about page//
 
-  items.push(item);
-
-  res.redirect("/");
-
+app.get("/about", function(req,res){
+  res.render("about");
 });
-
 
 app.listen(3000, function() {
   console.log("Server started on part 3000");
